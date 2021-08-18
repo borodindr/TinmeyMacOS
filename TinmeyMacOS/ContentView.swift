8e067b5c4600d8f92e381ffa44c4558b9fe504d1
Дмитрий Бородин <borodin.dr@pecom.ru>
2021-08-18 14:16:21 +0300

    Initial Commit

---
+++ TinmeyMacOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TinmeyMacOS
+//
+//  Created by Dmitry Borodin on 18.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TinmeyMacOS/TinmeyMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinmeyMacOSApp.swift
+//  TinmeyMacOS
+//
+//  Created by Dmitry Borodin on 18.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct TinmeyMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
